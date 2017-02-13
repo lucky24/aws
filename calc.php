9034f6f294aff33aeb9ce279c48078cf7818d622
lucky24 <ararakitom118@gmail.com>
2017-02-13 17:32:18 +0900

    課題3 calc.php

--- calc.php
+++ calc.php
@@ -1,7 +1,12 @@
 <?php
-$ans = 0;
 $string =  $_SERVER['QUERY_STRING'];
-
-
-echo $string;
-?>
+for($i = 0; $i < strlen($string); $i++) {
+  if (!in_array($string[$i], array(
+    '+', '-', '*', '/', '(', ')',
+    '0', '1', '2', '3', '4',
+    '5', '6', '7', '8', '9'
+  ))) {
+    die("ERROR");
+  }
+}
+echo eval("echo $string;") . "\n";
