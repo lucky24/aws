819c97c41cb45b44d4fbc9df6f06e9c9fa86ba74
lucky24 <ararakitom118@gmail.com>
2017-02-13 17:35:43 +0900

    errorの後に改行できるようにしました！

--- calc.php
+++ calc.php
@@ -6,7 +6,7 @@ for($i = 0; $i < strlen($string); $i++) {
     '0', '1', '2', '3', '4',
     '5', '6', '7', '8', '9'
   ))) {
-    die("ERROR");
+    die("ERROR\n");
   }
 }
 echo eval("echo $string;") . "\n";
